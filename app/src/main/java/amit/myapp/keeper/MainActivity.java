2f1728be86c302ff14d9d0c26252589d9d18d56f
Amit Assa <amitassa9@gmail.com>
2023-02-24 15:42:05 +0200

    progress

--- app/src/main/java/amit/myapp/keeper/MainActivity.java
+++ app/src/main/java/amit/myapp/keeper/MainActivity.java
@@ -1,12 +1,17 @@
 package amit.myapp.keeper;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDirections;
@@ -16,6 +21,8 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import amit.myapp.keeper.databinding.ActivityMainBinding;
+import amit.myapp.keeper.ui.Incidents.IncidentsFragment;
+import amit.myapp.keeper.ui.messages.MessagesFragment;
 import amit.myapp.keeper.ui.messages.MessagesFragmentDirections;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,27 +31,49 @@ public class MainActivity extends AppCompatActivity {
     Button btnLogOut;
     FirebaseAuth mAuth;
     NavController navController;
+    BottomNavigationView navView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        IncidentsFragment incidentsFragment = new IncidentsFragment();
+        MessagesFragment messagesFragment = new MessagesFragment();
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         mAuth = FirebaseAuth.getInstance();
+        Log.d("aa", "onCreate: ");
+        navView = findViewById(R.id.bottom_nav_bar);
+        navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                Log.d("click", "onNavigationItemSelected: aa");
+                switch (item.getItemId()){
+                    case R.id.incidentsFragment:
+                        //getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_activity_main, incidentsFragment).commit();
+                        Log.d("click", "onNavigationItemSelected: fuck");
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+                        return true;
+                    case R.id.messagesFragment:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_activity_main, messagesFragment).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.incidentsFragment, R.id.messagesFragment)
-                .build();
-        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
-        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        navController = navHostFragment.getNavController();
-        NavigationUI.setupActionBarWithNavController(this, navController);//, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.incidentsFragment, R.id.messagesFragment)
+//                .build();
+//        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
+//        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+//        navController = navHostFragment.getNavController();
+//        //NavigationUI.setupActionBarWithNavController(this, navController);//, appBarConfiguration);
+//        NavigationUI.setupWithNavController(binding.navView, navController);
 
 
     }
